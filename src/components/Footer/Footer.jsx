436c8be7892529911d53aeaf719f2672821a9b6b
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2022-12-23 12:49:28 +0500

    header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,108 @@
 import React from "react";
 
+// styles
+import "./footer.css";
+
+// reactstrap
+import { Col, Container, Row, ListGroup, ListGroupItem } from "reactstrap";
+
+// router
+import { Link } from "react-router-dom";
+
+// assets
+import logo from "../../assets/images/eco-logo.png";
+
 export const Footer = () => {
-  return <h1>Footer</h1>;
+  const year = new Date().getFullYear();
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4">
+            <div className="logo">
+              <img src={logo} alt="logo" />
+              <div>
+                <h1 className="text-white">Multimart</h1>
+              </div>
+            </div>
+            <p className="footer__text mt-4">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              quia, cupiditate ad at repellat rerum consectetur beatae quis
+              neque, voluptas similique temporibus totam harum, sint explicabo
+              numquam id veritatis optio?
+            </p>
+          </Col>
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="footer__quick-title">Top Category</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#"> Mobile Phones</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#"> Modern Sofa</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Arm Chair</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Smart Watches</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="2">
+            <div className="footer__quick-links">
+              <h4 className="footer__quick-title">Useful Links</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/shop">Shop</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/cart">Cart</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/login">Login</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Privacy Policy</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="footer__quick-title">Contact</h4>
+              <ListGroup className="footer__contact">
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-map-pin-line"></i>
+                  </span>
+                  <p>123 ZindaBazar, Sylhet, Bangladesh</p>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-phone-line"></i>
+                  </span>
+                  <p>+088123456789</p>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-mail-line"></i>
+                  </span>
+                  <p>dog@gmail.com</p>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="12">
+            <p className="footer__copyright">
+              Copyright {year} developer by ... . All rights reserved.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
