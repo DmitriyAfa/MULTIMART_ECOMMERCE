5c0324dd3f6a0bb56fba12cce02d3b007a90a578
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2023-01-04 09:02:43 +0500

    Firebase/signup and login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,49 +6,90 @@ import "../styles/login.css";
 // components
 import { Helmet } from "../components/Helmet/Helmet";
 
+// animation
+import { toast } from "react-toastify";
+
 // react-strap
 import { Container, Row, Col, Form, FormGroup } from "reactstrap";
 
 // router
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+// firebase
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase.config";
 
 export const Login = () => {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+
+  const navigate = useNavigate();
+
+  const signin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const { user } = userCredential;
+
+      setLoading(false);
+      toast.success("logged in");
+      navigate("/checkout");
+    } catch (error) {
+      setLoading(false);
+      toast.error(error.message);
+    }
+  };
   return (
     <Helmet title={"Login"}>
       <section>
         <Container>
           <Row>
-            <Col lg="6" className="m-auto text-center">
-              <h3 className="fw-bold mb-4">Login</h3>
-              <Form className="auth__form">
-                <FormGroup className="form__group">
-                  <input
-                    type="email"
-                    placeholder="Enter your email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </FormGroup>
-                <FormGroup className="form__group">
-                  <input
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    type="password"
-                    placeholder="Enter your password"
-                  />
-                </FormGroup>
+            {loading ? (
+              <Col lg="12" className="text-center">
+                <h5 className="fw-bold">Loading.....</h5>
+              </Col>
+            ) : (
+              <Col lg="6" className="m-auto text-center">
+                <h3 className="fw-bold mb-4">Login</h3>
+                <Form className="auth__form">
+                  <FormGroup className="form__group">
+                    <input
+                      type="email"
+                      placeholder="Enter your email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </FormGroup>
+                  <FormGroup className="form__group">
+                    <input
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      type="password"
+                      placeholder="Enter your password"
+                    />
+                  </FormGroup>
 
-                <button type="submit" className="buy__btn auth__btn">
-                  Login
-                </button>
-                <p>
-                  Don't have an account ?{" "}
-                  <Link to="/signup">Create an account</Link>
-                </p>
-              </Form>
-            </Col>
+                  <button
+                    onClick={signin}
+                    type="submit"
+                    className="buy__btn auth__btn"
+                  >
+                    Login
+                  </button>
+                  <p>
+                    Don't have an account ?{" "}
+                    <Link to="/signup">Create an account</Link>
+                  </p>
+                </Form>
+              </Col>
+            )}
           </Row>
         </Container>
       </section>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,15 +6,22 @@ import "../styles/login.css";
 // components
 import { Helmet } from "../components/Helmet/Helmet";
 
+// animation
+import { toast } from "react-toastify";
+
 // react-strap
 import { Container, Row, Col, Form, FormGroup } from "reactstrap";
 
 // router
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 // firebase
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../firebase.config";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { storage } from "../firebase.config";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../firebase.config";
 
 export const Signup = () => {
   const [userName, setUserName] = React.useState("");
@@ -22,6 +29,9 @@ export const Signup = () => {
   const [password, setPassword] = React.useState("");
   const [file, setFile] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
+
+  // router
+  const navigate = useNavigate();
 
   // firebase
   const signup = async (e) => {
@@ -36,8 +46,48 @@ export const Signup = () => {
       );
       const { user } = userCredential;
 
-      console.log(user);
-    } catch (error) {}
+      /*
+        метод ref()
+       Returns a StorageReference for the given url.
+       Возвращает StorageReference для данного URL-адреса.
+
+       метод uploadBytesResumable()
+        Uploads data to this object's location. The upload can be paused and resumed, and exposes progress updates.
+        Загружает данные в местоположение этого объекта. Загрузку можно приостановить и возобновить, а также отображать обновления о ходе выполнения.
+      */
+      const storageRef = ref(storage, `iamges/${Date.now() + userName}`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        (error) => {
+          toast.error(error.message);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downoloadURL) => {
+            // update user profile
+            await updateProfile(user, {
+              displayName: userName,
+              photoURL: downoloadURL,
+            });
+
+            // store user data in firestore database
+            await setDoc(doc(db, "users", user.uid), {
+              uid: user.uid,
+              displayName: userName,
+              email,
+              photoURL: downoloadURL,
+            });
+          });
+        }
+      );
+
+      setLoading(false);
+      toast.success("Account created");
+      navigate("/login");
+    } catch (error) {
+      setLoading(false);
+      toast.error("something went wrong");
+    }
   };
 
   return (
@@ -45,48 +95,54 @@ export const Signup = () => {
       <section>
         <Container>
           <Row>
-            <Col lg="6" className="m-auto text-center">
-              <h3 className="fw-bold mb-4">Signup</h3>
-              <Form className="auth__form" onSubmit={signup}>
-                <FormGroup className="form__group">
-                  <input
-                    type="text"
-                    placeholder="Username"
-                    value={userName}
-                    onChange={(e) => setUserName(e.target.value)}
-                  />
-                </FormGroup>
-                <FormGroup className="form__group">
-                  <input
-                    type="email"
-                    placeholder="Enter your email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </FormGroup>
-                <FormGroup className="form__group">
-                  <input
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    type="password"
-                    placeholder="Enter your password"
-                  />
-                </FormGroup>
-                <FormGroup className="form__group">
-                  <input
-                    onChange={(e) => setFile(e.target.files[0])}
-                    type="file"
-                  />
-                </FormGroup>
+            {loading ? (
+              <Col lg="12" className="text-center">
+                <h5 className="fw-bold">Loading.....</h5>
+              </Col>
+            ) : (
+              <Col lg="6" className="m-auto text-center">
+                <h3 className="fw-bold mb-4">Signup</h3>
+                <Form className="auth__form" onSubmit={signup}>
+                  <FormGroup className="form__group">
+                    <input
+                      type="text"
+                      placeholder="Username"
+                      value={userName}
+                      onChange={(e) => setUserName(e.target.value)}
+                    />
+                  </FormGroup>
+                  <FormGroup className="form__group">
+                    <input
+                      type="email"
+                      placeholder="Enter your email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </FormGroup>
+                  <FormGroup className="form__group">
+                    <input
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      type="password"
+                      placeholder="Enter your password"
+                    />
+                  </FormGroup>
+                  <FormGroup className="form__group">
+                    <input
+                      onChange={(e) => setFile(e.target.files[0])}
+                      type="file"
+                    />
+                  </FormGroup>
 
-                <button type="submit" className="buy__btn auth__btn">
-                  Create an Account
-                </button>
-                <p>
-                  Already have an account ? <Link to="/login">Login</Link>
-                </p>
-              </Form>
-            </Col>
+                  <button type="submit" className="buy__btn auth__btn">
+                    Create an Account
+                  </button>
+                  <p>
+                    Already have an account ? <Link to="/login">Login</Link>
+                  </p>
+                </Form>
+              </Col>
+            )}
           </Row>
         </Container>
       </section>
